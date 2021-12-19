b7bb0e83549092a419a9745b05bf02ccde376542
dnbworks <dooyongnsaakobalderama@gmail.com>
2021-12-19 21:47:25 +0800

    bug tracking and fixing in core

---
+++ src/Magma/Twig/TwigExtension.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Magma\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
+
+class TwigExtension extends AbstractExtension implements GlobalsInterface
+{
+
+}
